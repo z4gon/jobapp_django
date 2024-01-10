24bd9994781b5f7b8f5d7c9d5d03956d16351e4a
z4gon <4588601+z4gon@users.noreply.github.com>
2024-01-10 16:38:53 -0300

    dynamic url

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from app.views import hello
+# from app.views import hello, job_detail
+from app import views
 
 urlpatterns = [
-    path('', hello)
+    path('', views.hello),
+    path('job/<int:job_id>', views.job_detail)
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse("Hello World")
+
+def job_detail(request, job_id):
+    return HttpResponse(f"Job details for: {job_id = }")
