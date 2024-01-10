09015ef417d464bdf63c5d38c6e03d5a139c9997
z4gon <4588601+z4gon@users.noreply.github.com>
2024-01-10 16:45:53 -0300

    return html

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("<h3>Hello, World!</h3>")
 
 def job_detail(request, job_id):
-    return HttpResponse(f"Job details for: {job_id = }")
+    return HttpResponse(f"<h1>Job details for: {job_id}</h1>")
