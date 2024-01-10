55c65a59287df1e8f519230fd3bfbeff43f8b39f
z4gon <4588601+z4gon@users.noreply.github.com>
2024-01-10 17:27:08 -0300

    jobs dict

--- app/views.py
+++ app/views.py
@@ -3,12 +3,37 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+all_jobs = {
+    1: {
+        "id": 1,
+        "title": "Python Developer",
+        "company": "Google",
+        "location": "Mountain View, CA"
+    },
+    2: {
+        "id": 2,
+        "title": "Java Developer",
+        "company": "Facebook",
+        "location": "Menlo Park, CA"
+    },
+    3: {
+        "id": 3,
+        "title": "Ruby Developer",
+        "company": "Amazon",
+        "location": "Seattle, WA"
+    }
+}
+
 def hello(request):
     return HttpResponse("<h3>Hello, World!</h3>")
 
 def job_detail(request, job_id):
+    job = all_jobs.get(job_id)
     url = f"https://www.linkedin.com/jobs/view/{job_id}"
+    
     return HttpResponse(f"""
-        <h1>Job details for: {job_id}</h1>
+        <h1>{job.get("title")}</h1>
+        <h2>{job.get("company")}</h2>
+        <p>{job.get("location")}</p>
         Visit <a href={url}>this link</a> for more info.
     """)
