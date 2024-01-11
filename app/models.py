9c5dbe0c4b16bc278f59bc3317b3532ad85789ed
z4gon <4588601+z4gon@users.noreply.github.com>
2024-01-11 14:36:08 -0300

    add new fields to model

--- app/models.py
+++ app/models.py
@@ -6,3 +6,5 @@ class Job(models.Model):
     title = models.CharField(max_length=200)
     company = models.CharField(max_length=200)
     description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    salary = models.IntegerField()
