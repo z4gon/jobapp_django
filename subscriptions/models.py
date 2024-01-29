bb902a8aaa372e37e4d8cc24c73d6fb9d84c63a4
z4gon <4588601+z4gon@users.noreply.github.com>
2024-01-29 18:47:03 -0300

    NEWSLETTER_OPTION

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+NEWSLETTER_OPTION = [
+    ('W', 'Weekly'),
+    ('M', 'Monthly'),
+    ('N', 'Never')
+]
+
 # subscriber model
 class Subscriber(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
+    option = models.CharField(max_length=1, choices=NEWSLETTER_OPTION, default='W')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.email})"
