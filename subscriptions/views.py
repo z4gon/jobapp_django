e356757156ef5ee8ec02aba30c5a6890ddc7b620
z4gon <4588601+z4gon@users.noreply.github.com>
2024-01-29 15:19:28 -0300

    get the values from the POST dict

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def subscribe(request):
+    if request.POST:
+        # get first name, last name and email from the post request
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        print(f"POST REQUEST {email = } {firstname = } {lastname = }")
+
     context = {}
     return render(request, 'subscriptions/subscribe.html', context)
