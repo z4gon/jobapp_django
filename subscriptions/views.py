a8c166b716a74a6f3c1bdb6fce530e1dc9ace40d
z4gon <4588601+z4gon@users.noreply.github.com>
2024-01-29 15:26:14 -0300

    manual server side validation

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -3,12 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def subscribe(request):
+    context = {}
+
     if request.POST:
+
         # get first name, last name and email from the post request
         firstname = request.POST.get('firstname')
         lastname = request.POST.get('lastname')
         email = request.POST.get('email')
+
         print(f"POST REQUEST {email = } {firstname = } {lastname = }")
 
-    context = {}
+        # check for errors
+        errors = []
+        if email == "":
+            errors.append("Email is required")
+        if firstname == "":
+            errors.append("First name is required")
+        if lastname == "":
+            errors.append("Last name is required")
+        context["errors"] = errors
+
     return render(request, 'subscriptions/subscribe.html', context)
