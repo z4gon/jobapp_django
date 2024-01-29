3afe67a12ea162dab4781733f737699273bec301
z4gon <4588601+z4gon@users.noreply.github.com>
2024-01-29 15:31:26 -0300

    subscriber model and migration

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# subscriber model
+class Subscriber(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
